137aa66e87e397e7ccbfbec614bc87f8dfb11268
Ricardo <ricardosaldanha2005@gmail.com>
2026-01-17 23:28:57 +0000

    Avoid BigInt literals in constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@ export const BORROW_CANDIDATES = ["WMATIC", "LINK", "WETH"] as const;
 export const DEFAULT_HF_MIN = 1.8;
 export const DEFAULT_HF_MAX = 2.2;
 
-export const RAY = 10n ** 27n;
-export const WAD = 10n ** 18n;
+export const RAY = BigInt(10) ** BigInt(27);
+export const WAD = BigInt(10) ** BigInt(18);
